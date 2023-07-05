312a82780ec61ec67eb54eb37efb8d4da8d21f6c
Nelson An <hoangquangan63@gmail.com>
2023-07-05 22:22:35 +0700

    Second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <script setup>
-    import {computed} from 'vue'
+    import {computed, onMounted} from 'vue'
     import store from '../store'
+    import axiosClient from '../axiosClient'
+    const letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-    const meals = computed(()=>store.state.meals)
+    onMounted(() => {
+        axiosClient.get('/list.php?i=list')
+            .then(function(response){
+                console.log(response);
+            })
+
+    })
 </script>
 
 <template>
-    <pre>
-        {{meals}}
-    </pre>
+    <div class="flex flex-col p-8 ">
+        <div>
+            <input type="text" name="" id="" placeholder="Search for meals" class="rounded border-grey-200 border-2 w-full"/>
+        </div>
+
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link :to="{name:'byLetter', params:{letter:letter}}" v-for="letter in letters" :key="letter">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
+    
 </template>
 
 <style>
-
+  
 </style>
